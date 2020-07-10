f52fa9ab47b61cc2a11743887e26b55bd3c595bc
josh <josh.fierrosga@gmail.com>
2020-07-10 18:27:02 -0500

    initial commit for tandon program

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    string greeting = "Hello NYU Tandon!";
+    // different way of doing an \n
+    cout << greeting << endl;
+    cout << 1 + 1 << endl;
+    return 0;
+}
