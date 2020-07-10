81f2b7a4d6844b51885e6a9832650b682cb6b447
josh <josh.fierrosga@gmail.com>
2020-07-10 18:45:31 -0500

    created first c++ function. very similar to java.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 #include <vector>
 using namespace std;
 
+/*
+first function in c++
+takes a user input and displays it back in the terminal
+*/
+void getUserInput() {
+    string userInput;
+    cout << "waiting for your input.. \n";
+    cin >> userInput;
+    cout << "you typed: " + userInput <<endl;
+}
+
 int main() {
     string greeting = "Hello NYU Tandon!";
     // different way of doing an \n
     cout << greeting << endl;
     cout << 1 + 1 << endl;
-    string userInput;
-    cout << "waiting for your input.. \n";
-    cin >> userInput;
-    cout << "you typed: " + userInput <<endl;
+    //calling helper function
+    getUserInput();
 
     return 0;
 }
