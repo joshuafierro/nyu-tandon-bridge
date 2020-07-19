5c955b2e0fac739c7e8e835c13a8622fddb7c4cc
josh <josh.fierrosga@gmail.com>
2020-07-18 23:37:06 -0500

    trying to save user input while looping through questions in question3 file.

--- question3.cpp
+++ question3.cpp
@@ -10,32 +10,41 @@ void totalTimeWorkedTogether(int johnDays, int johnHours, int johnMin, int billD
     int totalMin = combinedDaysIntoMin + combinedHoursIntoMin + johnMin + billMin;
 
     int days = totalMin / MIN_IN_A_DAY;
+    cout << "John: " << johnDays << " " << johnHours << " " << johnMin << " Bill: " << billDays << " " << billHours <<" " << billMin <<endl;
 }
 
 void coworkerBesties(){
     int johnDays, johnHours, johnMin;
     int billDays, billHours, billMin;
+    int myVars[3] = {johnDays, johnHours, johnMin};
     
-    cout << "Please enter the number of days John has worked:" <<endl;
-    cin>>johnDays;
-    cout << "Please enter the number of hours John has worked:" <<endl;
-    cin>>johnHours;
-    cout << "Please enter the number of minutes John has worked:" <<endl;
-    cin>>johnMin;
+    for(int i = 0; i < 3; i++){
+        // string name = {"John", "John", "John", "Bill", "Bill", "Bill"};
+        string timeMeasurement[3] = {"days", "hours", "minutes"};
+        cout << "Please enter the number of "<< timeMeasurement[i] <<" John "<<  "has worked:" <<endl;
+        cin>> myVars[i];
+    }
 
-    cout << "Please enter the number of days Bill has worked:" <<endl;
-    cin>>billDays;
-    cout << "Please enter the number of hours Bill has worked:" <<endl;
-    cin>>billHours;
-    cout << "Please enter the number of minutes Bill has worked:" <<endl;
-    cin>>billMin;
-    // for(int i = 0; i < 6; i++){
-    //     string name = {"John", "John", "John", "Bill", "Bill", "Bill"};
-    //     string timeMeasurement[3] = {"days", "hours", "minutes"};
-    //     for(int j = 0; i < name.length(); i++){
-    //         cout << "Please enter the number of "<< timeMeasurement[j] <<name[j]<<  "has worked:" <<endl;
-            
-    //     }
-        
-    // }
+    for(int i = 0; i < 3; i++){
+        // string name = {"John", "John", "John", "Bill", "Bill", "Bill"};
+        string timeMeasurement[3] = {"days", "hours", "minutes"};
+        cout << "Please enter the number of "<< timeMeasurement[i] <<" Bill "<<  "has worked:" <<endl;
+        cin>> myVars[i];
+    }
+
+    totalTimeWorkedTogether(johnDays, johnHours, johnMin, billDays, billHours, billMin);
 }
+
+    // cout << "Please enter the number of days John has worked:" <<endl;
+    // cin>>johnDays;
+    // cout << "Please enter the number of hours John has worked:" <<endl;
+    // cin>>johnHours;
+    // cout << "Please enter the number of minutes John has worked:" <<endl;
+    // cin>>johnMin;
+
+    // cout << "Please enter the number of days Bill has worked:" <<endl;
+    // cin>>billDays;
+    // cout << "Please enter the number of hours Bill has worked:" <<endl;
+    // cin>>billHours;
+    // cout << "Please enter the number of minutes Bill has worked:" <<endl;
+    // cin>>billMin;
