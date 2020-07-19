9f0713a4612005e5c05e75d418b72932471a3dcb
josh <josh.fierrosga@gmail.com>
2020-07-18 22:52:19 -0500

    starting question3

---
+++ question3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void coworkerBesties(){
+    int johnDays, johnHours, johnMin;
+    int billDays, billHours, billMin;
+}
