0b87bf2c22492a5ed1923a912b9975a49423b674
josh <josh.fierrosga@gmail.com>
2020-07-18 23:40:43 -0500

    starting question 4.

---
+++ question4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void doMaths(){
+    int num1, num2;
+    cout << "Please enter two positive integers, seperated by a space:" <<endl;
+    cin>> num1 >> num2;
+}
