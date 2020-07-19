59747e4a6705147a75e245972051669f4745ae93
josh <josh.fierrosga@gmail.com>
2020-07-18 21:01:43 -0500

    starting to build out totalMoney and money calculation functions

--- question1.cpp
+++ question1.cpp
@@ -3,6 +3,30 @@
 #include <vector>
 using namespace std;
 
-void testFunction(){
-    cout << "hello from question 1" << endl;
+double calculateTotalMoney(int quarters, int dimes, int nickels, int pennies){
+    // double total = 0;
+    // const double dollar = 100;
+    // (quarters * 4) +
+    // (dimes*10) +
+    // (nickels *5) +
+    // (pennies);
+    return 0;
+}
+
+void totalMoney(){
+    int quarters, dimes, nickels, pennies;
+    double total = 0;
+    cout << "Please enter number of coins:" << endl;
+    cout << "# of quarters?" <<endl;
+    cin>>quarters;
+    cout << "# dimes?" <<endl;
+    cin>>dimes;
+    cout << "# nickels?" <<endl;
+    cin>>nickels;
+    cout << "# pennies?" <<endl;
+    cin>>pennies;
+
+
+
+    cout <<"Your total balance is: " << quarters << " dollars and " << total << " cents." <<endl;
 }
