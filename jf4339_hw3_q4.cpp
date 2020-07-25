53ec8cf2b261e8234f19ffe4111e6dd98cbd5881
josh <josh.fierrosga@gmail.com>
2020-07-25 16:03:51 -0500

    hw3 question 4 completed

--- jf4339_hw3_q4.cpp
+++ jf4339_hw3_q4.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main() {
-    
+    const int FLOOR_ROUND = 1;
+    const int CEILING_ROUND = 2;
+    const int ROUND = 3;
+    int userInput;
+    double usersRationalNumber;
+
+    cout<< "Please enter a Real number: "<<endl;
+    cin>> usersRationalNumber;
+    cout<< "Choose your rounding method:"<<endl;
+    cout<< "1. Floor Round"<<endl;
+    cout<< "2. Ceiling Round"<<endl;
+    cout<< "3. Round to the nearest whole number"<<endl;
+    cin>> userInput;
+
+    switch (userInput)
+    {
+    case FLOOR_ROUND:
+        cout<<floor(usersRationalNumber)<<endl;
+        break;
+    case CEILING_ROUND:
+        cout<<ceil(usersRationalNumber)<<endl;
+        break;
+    case ROUND:
+        cout<<round(usersRationalNumber)<<endl;
+        break;
+    default:
+        break;
+    }
+
     return 0;
 }
