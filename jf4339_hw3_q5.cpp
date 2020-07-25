2f8c652960b5a420427116cee768e954109cb4f9
josh <josh.fierrosga@gmail.com>
2020-07-25 16:09:41 -0500

    starting question 5

---
+++ jf4339_hw3_q5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double weight;
+    double height;
+    cout<<"Please enter weight (in pounds): ";
+    cin>> weight;
+
+    cout<<"Please enter height (in inches): ";
+    cin>> height;
+    double weightStatus;
+    cout<<"The weight status is: " << weightStatus <<endl;
+    return 0;
+}
