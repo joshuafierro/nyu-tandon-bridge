d8cba4dcdca3069612c8e1694d289c2a791a6432
josh <josh.fierrosga@gmail.com>
2020-07-31 23:45:24 -0500

    starting question 2

---
+++ jf4339_hw4_q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int usersDecimal;
+    string romanNumeral;
+
+    cout << "Enter a decimal number:"<< endl;
+    cin >> usersDecimal;
+    cout << usersDecimal << " is " << romanNumeral << endl;
+}
