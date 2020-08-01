a2e79282affa51005b2b1332b11d3f92ce945979
josh <josh.fierrosga@gmail.com>
2020-07-31 23:22:37 -0500

    Starting hw4 question 1

---
+++ jf4339_hw4_q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int usersInteger;
+    cout << "Please enter a positive integer: ";
+    cin >> usersInteger;
+    
+}
