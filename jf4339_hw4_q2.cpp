12373ece6f9d333fe75f79148d8a33dc2e868ee4
josh <josh.fierrosga@gmail.com>
2020-08-01 00:05:23 -0500

    hw4 q2: added functionality for looping over and properly subtracting from users input

--- jf4339_hw4_q2.cpp
+++ jf4339_hw4_q2.cpp
@@ -10,5 +10,37 @@ int main() {
 
     cout << "Enter a decimal number:"<< endl;
     cin >> usersDecimal;
-    cout << usersDecimal << " is " << romanNumeral << endl;
+    // while loop with if else checking if a certain number can subtract into it
+    const int usersOriginalDecimal = usersDecimal;
+    while(usersDecimal > 0){
+        if((usersDecimal - 1000) >= 0){
+            usersDecimal -= 1000;
+            cout << "subtracted properly by 1000" << endl;
+        }
+        else if((usersDecimal - 500) >= 0) {
+            usersDecimal -= 500;
+            cout << "subtracted properly by 500" << endl;
+        }
+        else if((usersDecimal - 100) >= 0){
+            usersDecimal -= 100;
+            cout << "subtracted properly by 100" << endl;
+        }
+        else if((usersDecimal - 50) >= 0) {
+            usersDecimal -= 50;
+            cout << "subtracted properly by 50" << endl;
+        }
+        else if((usersDecimal - 10) >= 0) {
+            usersDecimal -= 10;
+            cout << "subtracted properly by 10" << endl;
+        }
+        else if((usersDecimal - 5) >= 0) {
+            usersDecimal -= 5;
+            cout << "subtracted properly by 5" << endl;
+        }
+        else if((usersDecimal - 1) >= 0) {
+            usersDecimal -= 1;
+            cout << "subtracted properly by 1" << endl;
+        }
+    }
+    cout << usersOriginalDecimal << " is " << romanNumeral << endl;
 }
