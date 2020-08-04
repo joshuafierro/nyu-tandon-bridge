5fb7dcb044763bac1862d9acb3e3810e21af6e99
josh <josh.fierrosga@gmail.com>
2020-08-04 18:55:42 -0500

    starting question 6 for hw 4

---
+++ jf4339_hw4_q6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main() { 
+    int usersInput;
+    cout << "Please enter a positive integer: ";
+    cin >> usersInput;
+}
