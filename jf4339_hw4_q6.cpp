7eb5e40a86606126de3eaa15fcb9fc1cb95fc276
josh <josh.fierrosga@gmail.com>
2020-08-04 19:02:49 -0500

    Have functionality for question 6 so that we loop over a user provided integer, only printing out even numbers.

--- jf4339_hw4_q6.cpp
+++ jf4339_hw4_q6.cpp
@@ -8,4 +8,10 @@ int main() {
     int usersInput;
     cout << "Please enter a positive integer: ";
     cin >> usersInput;
+
+    for(int i = 1; i < usersInput; i ++) {
+        if(i % 2 == 0) {
+            cout << i << endl;
+        }
+    }
 }
