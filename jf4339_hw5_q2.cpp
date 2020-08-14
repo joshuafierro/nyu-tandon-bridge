0831be2b3fa6a57cccecd7c7f6180be49c8a785e
josh <josh.fierrosga@gmail.com>
2020-08-14 11:14:16 -0500

    Completed hw5 question 2 along with edge cases.

--- jf4339_hw5_q2.cpp
+++ jf4339_hw5_q2.cpp
@@ -13,36 +13,40 @@ int main() {
     srand(time(0));
     int myRandomNumber = rand() % 100 + 1;
     int attemptsLeft = 5;
-    // bool gameOver;
     int rangeX = 1;
     int rangeY = 100;
-    cout << myRandomNumber <<endl;
+    cout<< myRandomNumber <<endl;
 
     cout<< "I thought of a number between " << rangeX << " and " << rangeY <<"!" <<endl;
     for(int i = 0; i < 5; i++){
-        cout << "Try to guess it. Range: ["<<rangeX<<","<< rangeY<<"], Number of guesses left: " << attemptsLeft <<endl;
 
+        cout<< "Try to guess it. Range: ["<<rangeX<<","<< rangeY<<"], Number of guesses left: " << attemptsLeft <<endl;
+
+        attemptsLeft--;
         int numberOfGuesses = i + 1;
         int usersGuess;
         cin >> usersGuess;
 
         if(usersGuess == myRandomNumber) {
-            cout<<"Congrats! You guessed my number in " << numberOfGuesses << " guesses" <<endl;
+            cout<< "Congrats! You guessed my number in " << numberOfGuesses << " guesses" <<endl;
             break;
         }
         else if(attemptsLeft == 0) {
             cout<< "Out of guesses! My number was " << myRandomNumber <<endl;
         }
         else if(usersGuess < myRandomNumber) {
-            cout << "Your guess: " << usersGuess << endl;
+            cout<< "Your guess: " << usersGuess <<endl;
             cout<< "Wrong! My number is bigger."<<endl;
-            rangeX = usersGuess + 1;
+            if(usersGuess > rangeX){
+                rangeX = usersGuess + 1;
+            }
         }
         else if(usersGuess > myRandomNumber) {
-            cout << "Your guess: " << usersGuess << endl;
+            cout<< "Your guess: " << usersGuess <<endl;
             cout<< "Wrong! My number is smaller."<<endl;
-            rangeY = usersGuess - 1;
+            if(usersGuess < rangeY){
+                rangeY = usersGuess - 1;
+            }
         }
-        attemptsLeft--;
     }
 }
