154efe9d539e529e2700deb602f9458524bf078a
josh <josh.fierrosga@gmail.com>
2020-08-13 21:03:34 -0500

    stubbed out hw5 question 1

---
+++ jf4339_hw5_q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+/*
+Write a program that reads a positive integer n from the user and prints out a nxn
+multiplication table. The columns should be spaced by a tab.
+*/
+
+int main() {
+
+}
