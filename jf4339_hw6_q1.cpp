8d8d446492bbaddddcc5800d5ea3addad7a38a83
josh <josh.fierrosga@gmail.com>
2020-08-20 21:39:55 -0500

    finished hw6 q1

--- jf4339_hw6_q1.cpp
+++ jf4339_hw6_q1.cpp
@@ -3,8 +3,27 @@
 #include <vector>
 using namespace std;
 
+    // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+    /*
+        1. Write a function int fib(int n) that returns the n-th element of the Fibonacci sequence.
+        2. Write a program that prompts the user to enter a positive integer num, and then prints the 
+            num’s elements in the Fibonacci sequence.
+    */
+
 int fib(int n){
-    return n;
+
+    int index1 = 1, index2 = 1, element;
+
+    if(n < 3){
+        return 1;
+    } else {
+        for (int position = 3; position <= n; position++){
+            element = index1 + index2;
+            index1 = index2;
+            index2 = element;
+        }
+        return element;
+    }
 }
 
 int main(){
