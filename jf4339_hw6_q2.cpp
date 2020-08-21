459743fd6498b15bea01497a9390447c80a53fb9
josh <josh.fierrosga@gmail.com>
2020-08-21 14:45:23 -0500

    hw6 q2 is finished for the most part. printPineTree function is outputting an extra symbol.

--- jf4339_hw6_q2.cpp
+++ jf4339_hw6_q2.cpp
@@ -10,29 +10,14 @@ Your program should interact with the user to read the number of triangles in th
 the character filling the tree.
 */
 
-void printShiftedTriangle(int n, int m, char symbol){
-
-    for(int i = 1; i <= n; i++){
-        
-        for(int j = 0; j < m; j++){
-            cout<<"+";
-        }
-        for(int k = m; k > i; k--){
-            cout<<" ";
-        }
-         for(int l = 0; l < i; l++){
-            cout<<" "<<symbol;
-        }
-        cout<<endl;
-    }
-}
-void printPineTree(int n, char symbol){}
+void printShiftedTriangle(int n, int m, char symbol);
+void printPineTree(int n, char symbol);
 
 int main()
 {
     int n, m;
     char symbol;
-    
+
     cout<<"Number of lines: ";
     cin>>n;
     cout<<"Numer of shifted spaces: ";
@@ -40,5 +25,44 @@ int main()
     cout<<"Symbol: ";
     cin>>symbol;
     printShiftedTriangle(n, m, symbol);
+    cout<<endl;
+    cout<<endl;
+    printPineTree(n, symbol);
     return 0;
+}
+
+void printShiftedTriangle(int n, int m, char symbol){
+
+    for(int i = 1; i <= n; i++){
+        
+        // for(int j = 0; j < m; j++){
+        //     cout<<"+";
+        // }
+        for(int k = 1; k <= (n-i) + m; k++){
+            cout<<" ";
+        }
+         for(int l = 1; l <= (i*2) - 1; l++){
+            cout<<symbol;
+        }
+        cout<<endl;
+    }
+}
+void printPineTree(int n, char symbol){
+    // int numberOfTrees = n;
+
+    // while(numberOfTrees != 0){
+    //     for(int i = 0; i < n; i++){
+    //         for(int k = n; k > i; k--){
+    //             cout<<" ";
+    //         }
+    //         for(int l = 0; l < i; l++){
+    //             cout<<" "<<symbol;
+    //         }
+    //         cout<<endl;
+    //     }
+    //     numberOfTrees--;
+    // }
+    for(int i = 0; i<=n; i++) {
+        printShiftedTriangle(i+1,n-i,symbol);
+    }
 }
