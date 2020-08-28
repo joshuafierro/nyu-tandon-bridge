f264a6be794b9c42dcbe1b4e6d00deafe8ba2c46
josh <josh.fierrosga@gmail.com>
2020-08-28 13:06:09 -0500

    Stubbing out hw7 q1

---
+++ jf4339_hw7_q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+Implement a function:
+int printMonthCalender(int numOfDays, int startingDay)
+This function is given two parameters:
+numOfDays - The number of days in the month
+startingDay – a number 1-7 that represents the day in the week of the first day
+in that month (1 for Monday, 2 for Tuesday, 3 for Wednesday, etc.).
+ 
+The function should:
+Print a formatted monthly calendar of that month
+Return a number 1-7 that represents the day in the week of the last day in that 
+*/
+
+int main(){
+    return 0;
+}
