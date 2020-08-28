80fb90f502c050c1f97289e01a53cf86adfc5646
josh <josh.fierrosga@gmail.com>
2020-08-28 13:44:15 -0500

    Most functionality is complete. Need to work on formatting.

--- jf4339_hw7_q1.cpp
+++ jf4339_hw7_q1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 /*
 Implement a function:
@@ -15,6 +16,30 @@ Print a formatted monthly calendar of that month
 Return a number 1-7 that represents the day in the week of the last day in that 
 */
 
+int printMonthCalender(int numOfDays, int startingDay);
+
 int main(){
+    int numOfDays, startingDay;
+    cin>> numOfDays >> startingDay;
+    printMonthCalender(numOfDays, startingDay);
     return 0;
+}
+
+int printMonthCalender(int numOfDays, int startingDay) {
+    cout<<"Mon \t Tues \t Wed \t Thurs \t Fri \t Sat \t Sun"<<endl;
+    for(int i = 1; i <= numOfDays; i++ ){
+        if (i % 7 == 0){
+            cout<<endl;
+        }
+        cout<<i << "\t";
+    }
+    cout<<endl;
+    return numOfDays;
+}
+
+void myFunction(int numOfTabs){
+    while(numOfTabs > 0){
+        cout<<"\t"<<endl;
+        numOfTabs--;
+    }
 }
