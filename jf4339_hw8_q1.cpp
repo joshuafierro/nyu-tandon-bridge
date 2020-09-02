c3c945048fa0200d7d3739ea82667e91eedd5e73
josh <josh.fierrosga@gmail.com>
2020-09-01 22:44:21 -0500

    Most functionality for program is complete. Just need to return the indicies of the answer

--- jf4339_hw8_q1.cpp
+++ jf4339_hw8_q1.cpp
@@ -3,19 +3,32 @@
 #include <vector>
 using namespace std;
 
+static int SIZE_OF_ARRAY = 20;
+int minInArray(int arr[], int arrSize);
+
 int main(){
+    int numbers[SIZE_OF_ARRAY];
+    int userInput;
+
+    cout<<"Please enter 20 integers separated by a space: "<<endl;
+    for(int i = 0; i < SIZE_OF_ARRAY; i++){
+        cin >> userInput;
+        numbers[i] = userInput;
+    }
+    int answer = minInArray(numbers, SIZE_OF_ARRAY);
+    cout<<"The minimum value is "<<answer<<endl;
     return 0;
 }
 
 /*
 Implement the function: int minInArray(int arr[], int arrSize)
-This function is given arr, an array of integers, and its logical size, arrSize. When called,
-it returns the minimum value in arr.
+This function is given arr, an array of integers, and its logical size, arrSize. 
+When called, it returns the minimum value in arr.
 */
 int minInArray(int arr[], int arrSize){
-    int min;
-    for(int i; i < arrSize; i++){        
-        if(min < arr[i]){
+    int min = arr[0];
+    for(int i = 1; i < arrSize; i++){        
+        if(arr[i] < min){
             min = arr[i];
         }
     }
