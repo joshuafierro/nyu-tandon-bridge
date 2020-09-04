aa8174e7da87b2ee932bbe0f242d9660de9613a8
josh <josh.fierrosga@gmail.com>
2020-09-04 15:16:48 -0500

    printing random number and pin number for hw8 q4

--- jf4339_hw8_q4.cpp
+++ jf4339_hw8_q4.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 int const PIN = 45897;
+int arr[10];
 
 int main(){
     cout<<"PIN: ";
     for(int i = 0; i < 10; i++){
         cout<< i <<" ";
+        arr[i] = rand() % 9 + 1;
+    }
+    cout<<endl;
+    cout<<"NUM: ";
+    for(int i = 0; i < 10; i++){
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
     return 0;
