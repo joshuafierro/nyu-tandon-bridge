5d1d2ede0378c1c24272e6dd826e25a3d6d9dd58
josh <josh.fierrosga@gmail.com>
2020-09-04 15:41:51 -0500

    started hw8 q5

---
+++ jf4339_hw8_q5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    string firstName, middleName, lastName;
+    cout<<"Please enter your full legal name(including Middle Name or Inital): "<<endl;
+    cin>>firstName, middleName, lastName;
+    return 0;
+}
