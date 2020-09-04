a36df8166f22735aac015870a6cb8f7c13c7fcd0
josh <josh.fierrosga@gmail.com>
2020-09-04 15:47:56 -0500

    completed hw8 q5

--- jf4339_hw8_q5.cpp
+++ jf4339_hw8_q5.cpp
@@ -7,6 +7,7 @@ int main(){
     string firstName, middleName, lastName;
     cout<<"Please enter your full legal name(including Middle Name or Inital): "<<endl;
     cin>>firstName >> middleName >> lastName;
-    cout<<lastName<< ", " << firstName << " "<< middleName <<endl;
+
+    cout<<lastName<< ", " << firstName << " "<< middleName.substr(0,1) << "." <<endl;
     return 0;
 }
