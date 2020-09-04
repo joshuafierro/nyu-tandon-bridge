23069f14b3336daafb3ae5701ce0221ee2c4e206
josh <josh.fierrosga@gmail.com>
2020-09-04 14:34:02 -0500

    starting hw8 q4

---
+++ jf4339_hw8_q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int const PIN = 45897;
+
+int main(){
+    cout<<"PIN: ";
+    for(int i = 0; i < 10; i++){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
