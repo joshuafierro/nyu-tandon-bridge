dfd4e2122bb58a8e3ee55b0793925d9446bb5ad5
josh <josh.fierrosga@gmail.com>
2020-09-04 15:45:26 -0500

    Most functionality for hw8 q5 complete.

--- jf4339_hw8_q5.cpp
+++ jf4339_hw8_q5.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(){
     string firstName, middleName, lastName;
     cout<<"Please enter your full legal name(including Middle Name or Inital): "<<endl;
-    cin>>firstName, middleName, lastName;
+    cin>>firstName >> middleName >> lastName;
+    cout<<lastName<< ", " << firstName << " "<< middleName <<endl;
     return 0;
 }
