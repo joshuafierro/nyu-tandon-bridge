9fd1eb4bd3f1149bd8df88e9b005385c14e79305
josh <josh.fierrosga@gmail.com>
2020-09-04 16:04:54 -0500

    hw8 q6: most functionality is complete just need to find a better way to tokenize string

--- jf4339_hw8_q6.cpp
+++ jf4339_hw8_q6.cpp
@@ -7,6 +7,11 @@ int main(){
     string userInput;
     cout<<"Please enter a line of text:"<<endl; 
     getline(cin, userInput);
+    for(int i=0; i<userInput.length(); i++){
+        if(isdigit(userInput[i])){
+            userInput[i] = 'x';
+        }
+    }
     cout<<userInput<<endl;
     return 0;
 }
