86f104bfd9434a574bc2273940741d04ebf2e4fc
josh <josh.fierrosga@gmail.com>
2020-09-11 21:16:47 -0500

    stubbing out hw9 q4

--- jf4339_hw9_q4.cpp
+++ jf4339_hw9_q4.cpp
@@ -3,7 +3,20 @@
 #include <vector>
 using namespace std;
 
+void oddsKeepEvensFlip(int arr[], int arrSize);
+
 int main(){
     return 0;
 }
 
+/*
+This function gets an array of integers arr and its logical size arrSize. 
+When called, it will reorder the elements of arr so that:
+1. All the odd numbers come before all the even numbers
+2. The odd numbers will keep their original relative order
+3. The even numbers will be placed in a reversed order 
+(relative to their original order).
+*/
+void oddsKeepEvensFlip(int arr[], int arrSize){
+
+}
