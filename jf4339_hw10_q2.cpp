796e0df9fff825aa8e3e5fa943853879e6d9b102
josh <josh.fierrosga@gmail.com>
2020-09-18 09:04:35 -0500

    hw10: starting q2

---
+++ jf4339_hw10_q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int* findMissing(int arr[], int n, int& resArrSize);
+
+int main(){
+
+    return 0;
+}
+
+/*
+cCreates and returns a new array, that contains all the numbers in range {0, 1, 2, ... , n} 
+that are not in arr. The function also updates the output parameter, resArrSize, 
+with the logical size of the new array that was created.
+*/
+
+int* findMissing(int arr[], int n, int& resArrSize){
+    
+}
