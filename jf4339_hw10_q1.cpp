b3f797906c7f72b96311dc305ba6788e4dd678c6
josh <josh.fierrosga@gmail.com>
2020-09-17 21:29:23 -0500

    hw10: starting q1

---
+++ jf4339_hw10_q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
+/*
+When called, it will create and return a new array (of strings), 
+that contains all the words in sentence. 
+The function also updates the output parameter, outWordsArrSize, 
+with the logical size of the new array that was created.
+*/
+string* createWordsArray(string sentence, int& outWordsArrSize);
