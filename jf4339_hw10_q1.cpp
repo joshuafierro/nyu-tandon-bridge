0bc05e6e87001858abe4f132bd2257d46b8cf05a
josh <josh.fierrosga@gmail.com>
2020-09-18 00:11:33 -0500

    hw10: function can now split words within a string and save each word into an array. function also updates the outWordArrSize variable

--- jf4339_hw10_q1.cpp
+++ jf4339_hw10_q1.cpp
@@ -3,7 +3,14 @@
 #include <vector>
 using namespace std;
 
+string* createWordsArray(string sentence, int& outWordsArrSize);
+
 int main(){
+    string userInput;
+    int outWordsArrSize = 0;
+    getline(cin, userInput);
+    createWordsArray(userInput, outWordsArrSize);
+    cout<<outWordsArrSize<<endl;
     return 0;
 }
 
@@ -13,4 +20,24 @@ that contains all the words in sentence.
 The function also updates the output parameter, outWordsArrSize, 
 with the logical size of the new array that was created.
 */
-string* createWordsArray(string sentence, int& outWordsArrSize);
+string* createWordsArray(string sentence, int& outWordsArrSize){
+    vector<string> wordsArray;
+    string word = "";
+    for(auto x : sentence){
+        if(x == ' '){
+            // cout<<word<<endl;
+            wordsArray.push_back(word);
+            word ="";
+        }
+        else{
+            word += x;
+        }
+    }
+    wordsArray.push_back(word);
+    for(int i = 0; i < wordsArray.size(); i++){
+        cout<<wordsArray[i]<<endl;
+    }
+
+    outWordsArrSize = wordsArray.size();
+    // return wordsArray;
+}
